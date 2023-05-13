626bf27a1e45f27e71d7284f3903f98c382dec65
pgersl <pgersl05@gmail.com>
2023-05-13 16:03:18 +0200

    more testing

--- static/js/nav.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.querySelector('.toggle');
-const linksContainer = document.querySelector('.links');
-
-toggleButton.addEventListener('click', () => {
-  linksContainer.classList.toggle('show');
-});
