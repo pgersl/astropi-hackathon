9368d7f3f8a81e70e8f301141dbc47d6a222c68b
Petr Geršl <pgersl05@gmail.com>
2024-08-08 15:58:35 +0200

    dumb js fix

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -2,10 +2,34 @@ const dropdownLinks = document.querySelectorAll('.dropdown-link');
 
 dropdownLinks.forEach(dropdownLink => {
     const dropdownContent = dropdownLink.querySelector('.link-dropdown');
+    let isOpen = false;
+
     dropdownLink.addEventListener('mouseenter', () => {
         dropdownContent.style.display = 'flex';
     });
+
     dropdownLink.addEventListener('mouseleave', () => {
-        dropdownContent.style.display = 'none';
+        if (!isOpen) {
+            dropdownContent.style.display = 'none';
+        }
+    });
+
+    dropdownLink.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        if (isOpen) {
+            dropdownContent.style.display = 'none';
+        } else {
+            dropdownContent.style.display = 'flex';
+        }
+
+        isOpen = !isOpen;
+    });
+
+    document.addEventListener('click', (event) => {
+        if (!dropdownLink.contains(event.target)) {
+            dropdownContent.style.display = 'none';
+            isOpen = false;
+        }
     });
 });
